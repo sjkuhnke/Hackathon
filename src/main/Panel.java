949b4bd283b85137dcfcfafe9e7594c1ee79f2b6
shaej <shaej@172.23.100.23>
2023-09-23 17:16:33 -0500

    Fixed getHandValue(still buggy)

--- src/main/Panel.java
+++ src/main/Panel.java
@@ -7,6 +7,9 @@ import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import javax.swing.ImageIcon;
@@ -67,8 +70,8 @@ public class Panel extends JPanel {
 		community = new Card[5];
 		for (int i = 0; i < 5; i++) {
 			JLabel cardLabel = communityDisplay[i];
-			Card card = community[i];
-			card = dealCard();
+			Card card = dealCard();
+			community[i] = card;
 			cardLabel = new JLabel();
 			cardLabel.setIcon(new ImageIcon(card.icon));
 			
@@ -79,17 +82,26 @@ public class Panel extends JPanel {
 		communityPanel.setBounds(100, 100, 1000, 200);
 		add(communityPanel);
 		
-		addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                // Get the coordinates of the mouse click
-                int x = e.getX();
-                int y = e.getY();
-                
-                // Print the coordinates
-                System.out.println("Mouse Clicked at: X=" + x + ", Y=" + y);
-            }
-        });
+		ArrayList<Card> yourCards = new ArrayList<>();
+		for (Card card : community) {
+			yourCards.add(card);
+		}
+		yourCards.add(p.getCard1());
+		yourCards.add(p.getCard2());
+		
+		getBestHand(yourCards);
+		
+//		addMouseListener(new MouseAdapter() {
+//            @Override
+//            public void mouseClicked(MouseEvent e) {
+//                // Get the coordinates of the mouse click
+//                int x = e.getX();
+//                int y = e.getY();
+//                
+//                // Print the coordinates
+//                System.out.println("Mouse Clicked at: X=" + x + ", Y=" + y);
+//            }
+//        });
 	}
 	
 	private void updateCards() {
@@ -110,42 +122,110 @@ public class Panel extends JPanel {
 		g.drawImage(img, 0, 0, null);
 	}
 	
-	public Card[] getBestHand(Card[] cards) {
+	private Card[] getBestHand(ArrayList<Card> yourCards) {
 		Card[] result = new Card[5];
-		ArrayList<ArrayList<Card>> possibleHands = generateCombinations(cards);
+		ArrayList<ArrayList<Card>> possibleHands = generateCombinations(yourCards);
 		
+		Map<ArrayList<Card>, Integer> scores = new HashMap<>();
 		for (ArrayList<Card> hand : possibleHands) {
-			System.out.println(hand.toString());
+			scores.put(hand, getHandValue(hand));
 		}
+		
+		System.out.print(Collections.max(scores.values()));
 		
 		return result;
 	}
 
-	private ArrayList<ArrayList<Card>> generateCombinations(Card[] cards) {
+	private ArrayList<ArrayList<Card>> generateCombinations(ArrayList<Card> yourCards) {
 		ArrayList<ArrayList<Card>> combinations = new ArrayList<>();
 		
-		generateCombinationsHelper(cards, 0, new ArrayList<>(), combinations);
+		generateCombinationsHelper(yourCards, 0, new ArrayList<>(), combinations);
 
         return combinations;
 	}
 
-	private void generateCombinationsHelper(Card[] cards, int index, ArrayList<Card> currentCombination, ArrayList<ArrayList<Card>> combinations) {
+	private void generateCombinationsHelper(ArrayList<Card> yourCards, int index, ArrayList<Card> currentCombination, ArrayList<ArrayList<Card>> combinations) {
 		if (currentCombination.size() == 5) {
             combinations.add(new ArrayList<>(currentCombination));
             return;
         }
 
-        if (index >= cards.length) {
+        if (index >= yourCards.size()) {
             return;
         }
 
         // Include the current card in the combination
-        currentCombination.add(cards[index]);
-        generateCombinationsHelper(cards, index + 1, currentCombination, combinations);
+        currentCombination.add(yourCards.get(index));
+        generateCombinationsHelper(yourCards, index + 1, currentCombination, combinations);
         currentCombination.remove(currentCombination.size() - 1); // Backtrack
 
         // Exclude the current card from the combination
-        generateCombinationsHelper(cards, index + 1, currentCombination, combinations);
+        generateCombinationsHelper(yourCards, index + 1, currentCombination, combinations);
 		
 	}
+	
+	private int getHandValue(ArrayList<Card> cards) {
+	    Map<Integer, Integer> rankCount = new HashMap<Integer, Integer>();
+	    Map<String, Integer> suitCount = new HashMap<String, Integer>();
+
+	    // Count each suit
+	    for (Card card : cards) {
+	    	suitCount.put(card.suit, suitCount.getOrDefault(card.suit, 0) + 1);
+	    }
+
+	    // Count each rank
+	    for (Card card : cards) {
+	        rankCount.put(card.rank, rankCount.getOrDefault(card.rank, 0) + 1);
+	    }
+
+	    // Check for straight
+	    boolean isStraight = true;
+	    ArrayList<Integer> rankList = new ArrayList<Integer>(rankCount.keySet());
+	    Collections.sort(rankList);
+
+	    for (int i = 0; i < rankList.size() - 1; i++) {
+	        if (rankList.get(i + 1) != rankList.get(i) + 1) {
+	            isStraight = false;
+	            break;
+	        }
+	    }
+
+	    int maxRankCount = Collections.max(rankCount.values());
+	    int maxSuitCount = Collections.max(suitCount.values());
+
+	    // Check for straight flush
+	    if (maxSuitCount == 5 && isStraight) {
+	        return 9;
+	    }
+	    // Check for quads
+	    else if (maxRankCount == 4) {
+	        return 8;
+	    }
+	    // Check for full house
+	    else if (maxRankCount == 3 && Collections.frequency(rankCount.values(), 2) == 1) {
+	        return 7;
+	    }
+	    // Check for flush
+	    else if (maxSuitCount == 5) {
+	        return 6;
+	    }
+	    // Check for straight
+	    else if (isStraight) {
+	        return 5;
+	    }
+	    // Check for trips
+	    else if (maxRankCount == 3) {
+	        return 4;
+	    }
+	    // Check for two pair
+	    else if (Collections.frequency(rankCount.values(), 2) == 2) {
+	        return 3;
+	    }
+	    // Check for pair
+	    else if (maxRankCount == 2) {
+	        return 2;
+	    }
+	    return 1;
+	}
+
 }
