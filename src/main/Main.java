eb45aa723c3e8229d65c8ee89049ddd51cf83579
shaej <shaej@172.23.100.23>
2023-09-23 11:29:40 -0500

    Panel changes

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,14 @@ import javax.swing.*;
 
 class Main {
 	public static void main(String[] args) {
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("Poker Game");
+		
 		Panel gamePanel = new Panel();
+		
+		window.add(gamePanel);
+		window.pack();
 	}
 }
--- src/main/Panel.java
+++ src/main/Panel.java
@@ -1,5 +1,12 @@
 package main;
 
-public class Panel {
+import javax.swing.JFrame;
+
+public class Panel extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 }
